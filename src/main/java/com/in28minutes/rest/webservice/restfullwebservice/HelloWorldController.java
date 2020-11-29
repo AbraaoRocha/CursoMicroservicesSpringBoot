10739e1c66cd5f0d4d9ae7ff3c612425f69fdeef
Abraão Rocha <abraaorochafranca@gmail.com>
2020-11-29 14:55:06 -0300

    Finalização da parte do HELLOWORLD

---
+++ src/main/java/com/in28minutes/rest/webservice/restfullwebservice/HelloWorldController.java
@@ -0,0 +1,32 @@
+package com.in28minutes.rest.webservice.restfullwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	private static final String HELLO_WORLD = "Hello World";
+
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+
+		return HELLO_WORLD;
+
+	}
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+
+		return new HelloWorldBean(HELLO_WORLD);
+
+	}
+
+	@GetMapping("/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+
+		return new HelloWorldBean(String.format("Hello World  , %s", name));
+
+	}
+}
